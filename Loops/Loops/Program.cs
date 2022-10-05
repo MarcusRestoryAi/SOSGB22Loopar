d5618d03088bbc8ea1a5a2dc07a06e7746f1a4dc
Marcus <marcus.henriksson@restory.ai>
2022-10-05 11:41:40 +0200

    Person 2 has fixed the project for the better!

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -7,6 +7,7 @@
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Hello, World!");
+                Console.WriteLine("Person 2 is better than 1");
             }
         }
     }
